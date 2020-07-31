b71f12697da491c039a27677d3fbe6d039085aa0
yashpupneja <44907218+yashpupneja@users.noreply.github.com>
2020-07-31 23:14:36 +0530

    Add files via upload

---
+++ Arrays/Last Index of One.cpp
@@ -0,0 +1,47 @@
+// Last Index of One
+// Given a string S consisting only '0's and '1's,  print the last index of the '1' present in it.
+
+// Input:
+// First line of the input contains the number of test cases T, T lines follow each containing a stream of characters.
+
+// Output:
+// Corresponding to every test case, output the last index of 1. If 1 is not present, print "-1" (without quotes).
+
+// Constraints:
+// 1 <= T <= 110
+// 1 <= |S| <= 106
+
+// Example:
+// Input:
+// 2
+// 00001
+// 0
+// Output:
+// 4
+// -1
+
+// Explanation:
+// Testcase 1: Last index of  1 in given string is 4.
+// Testcase 2: Since, 1 is not present, so output is -1.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int flag=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='1'){
+                flag=1;
+                cout<<i<<endl;
+                break;
+            }
+        }
+        if(flag==0) cout<<-1<<endl;
+    }
+	//code
+	return 0;
+}
