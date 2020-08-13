b6ea63905b24395a24c19bc7abceb7b117386704
yashpupneja <44907218+yashpupneja@users.noreply.github.com>
2020-08-13 08:59:22 +0530

    Update SelectionSort.cpp

--- Coding Blocks/Arrays/SelectionSort.cpp
+++ Coding Blocks/Arrays/SelectionSort.cpp
@@ -1,3 +1,7 @@
+//Selection sort is used to sort randomly shuffled array in increasing/decreasing order. 
+//In this algorithm, we first find out the smallest element in the array and then swap it with the first element and then the remaining array is unsorted array. 
+//Now, starting from the second element,we perform the same procedure and hence elements will be sorted as needed.
+
 #include<bits/stdc++.h>
 using namespace std;
 void ss(int a[],int n){
